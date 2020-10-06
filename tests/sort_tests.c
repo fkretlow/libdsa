eb91e40f23f605d1bc5d01221502295f3bbf52fb
Florian Kretlow <floriankretlow@gmx.de>
2020-10-06 17:19:54 +0200

    test is_sorted, do multiple random runs for each algorithm

--- tests/sort_tests.c
+++ tests/sort_tests.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #include "debug.h"
 #include "sort.h"
 #include "test_utils.h"
 #include "unittest.h"
 
-#define N_ELEMENTS 1000
-#define MAX_VALUE 1000
+#define N_ELEMENTS 1024
+#define MAX_VALUE 100
+#define N_RUNS 100
 
 static int A[N_ELEMENTS];
 
-int test_quicksort(void)
+int test_is_sorted(void)
 {
     make_random(A, N_ELEMENTS, MAX_VALUE);
-    quicksort(A, N_ELEMENTS, sizeof(*A), compint);
-    test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint), "Quicksort: Not sorted.");
+    test(!is_sorted(A, N_ELEMENTS, sizeof(*A), compint),
+            "is_sorted returned true for unsorted array.");
+    make_equal(A, N_ELEMENTS, 0);
+    test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint),
+            "is_sorted returned false for array with equal values.");
+    make_sorted(A, N_ELEMENTS);
+    test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint),
+            "is_sorted returned false for sorted array.");
+    return TEST_OK;
+}
+
+int test_quicksort(void)
+{
+    for (int i = 0; i < N_RUNS; ++i) {
+        make_random(A, N_ELEMENTS, MAX_VALUE);
+        quicksort(A, N_ELEMENTS, sizeof(*A), compint);
+        test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint), "Quicksort: Not sorted.");
+    }
     quicksort(A, N_ELEMENTS, sizeof(*A), compint);
     test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint),
             "Quicksort: Not sorted after sorting already sorted array.");
@@ -25,9 +41,11 @@ int test_quicksort(void)
 
 int test_mergesort(void)
 {
-    make_random(A, N_ELEMENTS, MAX_VALUE);
-    mergesort(A, N_ELEMENTS, sizeof(*A), compint);
-    test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint), "Mergesort: Not sorted.");
+    for (int i = 0; i < N_RUNS; ++i) {
+        make_random(A, N_ELEMENTS, MAX_VALUE);
+        mergesort(A, N_ELEMENTS, sizeof(*A), compint);
+        test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint), "Mergesort: Not sorted.");
+    }
     mergesort(A, N_ELEMENTS, sizeof(*A), compint);
     test(is_sorted(A, N_ELEMENTS, sizeof(*A), compint),
             "Mergesort: Not sorted after sorting already sorted array.");
@@ -36,7 +54,9 @@ int test_mergesort(void)
 
 int main()
 {
+    srand((unsigned)time(NULL));
     test_suite_start();
+    run_test(test_is_sorted);
     run_test(test_quicksort);
     run_test(test_mergesort);
     test_suite_end();
--- tests/test_utils.h
+++ tests/test_utils.h
@@ -1,6 +1,9 @@
 #ifndef _test_utils_h
 #define _test_utils_h
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "debug.h"
 
 int compint(const int* a, const int* b)
@@ -10,16 +13,23 @@ int compint(const int* a, const int* b)
 
 void make_random(int* A, size_t n_members, unsigned max_value)
 {
-    for (size_t i = 0; i < n_members; ++i) {
-        A[i] = rand() % max_value;
-    }
+    for (size_t i = 0; i < n_members; ++i) A[i] = rand() % max_value;
+}
+
+void make_equal(int* A, size_t n_members, unsigned value)
+{
+    for (size_t i = 0; i < n_members; ++i) A[i] = value;
+}
+
+
+void make_sorted(int* A, size_t n_members)
+{
+    for (size_t i = 0; i < n_members; ++i) A[i] = i;
 }
 
 void print_array(int* A, size_t n_members)
 {
-    for (size_t i = 0; i < n_members; ++i) {
-        printf("%d ", A[i]);
-    }
+    for (size_t i = 0; i < n_members; ++i) printf("%d ", A[i]);
     printf("\n");
 }
 
